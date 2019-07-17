6cf23846dad446d97e7e000ec0863f1dd9e3bf6b
lilymzhou <lilyzhou@stanford.edu>
2019-07-17 13:09:22 -0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,28 +5,30 @@ import Database from './database';
 import Glossary from './glossary';
 import './App.css';
 
-function App() {
-  return (
-    <HashRouter>
-      <div>
-        <Route exact={true} path ='/' render={() => (
-          <div className="App">
-          <Home />
-          </div>
-        )} />
-        <Route exact={true} path = '/database' render={() => (
-          <div className="App">
-            <Database />
-          </div>
-        )} />
-        <Route exact={true} path = '/glossary' render={() => (
-          <div className="App">
-            <Glossary />
-          </div>
-        )} />
-      </div>
-    </HashRouter>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <Route exact={true} path ='/' render={() => (
+            <div className="App">
+            <Home />
+            </div>
+          )} />
+          <Route exact={true} path = '/database' render={() => (
+            <div className="App">
+              <Database />
+            </div>
+          )} />
+          <Route exact={true} path = '/glossary' render={() => (
+            <div className="App">
+              <Glossary />
+            </div>
+          )} />
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
